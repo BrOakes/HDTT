05326dcfdc8c0f219451a47096ca304506b8de00
Bradley Oakes <BrOakesGames@gmail.com>
2020-02-07 17:41:28 -0500

    multiple tool brands scraping capabilities

--- crawler.js
+++ crawler.js
@@ -1,6 +1,9 @@
 const request = require('request');
 const fs = require('fs');
 
+
+const toolListings = [];
+
 class ToolData{
 	constructor(name, model, price, rating, link) {
 		this.name = name;
@@ -13,26 +16,55 @@ class ToolData{
 	get allData() {
 		return `${this.name} ${this.model} ${this.price} ${this.rating} ${this.link}`
 	}
+
+	get getName() {
+		return this.name;
+	}
+
+	get getModel() {
+		return this.model;
+	}
+
+	get getPrice() {
+		return this.price;
+	}
+
+	get getLink() {
+		return this.link;
+	}
 }
 
 
-// const temp = new ToolData('saw', '2445', '199.00', '4.7', '249.99')
+function getToolListings(brand, start = 0) {
 
-// console.log(temp.allData);
+	let urlLink;
 
-const toolListings = [];
+	switch(brand){
+		case 'Milwaukee':
+			urlLink = 'https://www.homedepot.com/b/Tools/Milwaukee/N-5yc1vZc1xyZzv'
+			break;
+		case 'Makita':
+			urlLink = 'https://www.homedepot.com/b/Tools/Makita/N-5yc1vZc1xyZyg';
+			break;
+		case 'Dewalt':
+			urlLink = 'https://www.homedepot.com/b/Tools/DEWALT/N-5yc1vZc1xyZ4j2';
+			break;
+		case 'Hilti':
+			urlLink = 'https://www.homedepot.com/b/Tools/Hilti/N-5yc1vZc1xyZpx';
+			break;
+		case 'Bosch':
+			urlLink = 'https://www.homedepot.com/b/Tools/Bosch/N-5yc1vZc1xyZ9u';
+			break;
+	}
 
-
-function getToolListings(start = 0) {
 	request({
-		uri: "https://www.homedepot.com/b/Tools/N-5yc1vZc1xyZzv?experienceName=default&Nao=" + start +"&Ns=None",
+		uri: urlLink + "?Nao=" + start +"&Ns=None",
 	}, function (error, response, body) {
-			let totalResults = body.substring(body.indexOf('<span class="product-count__title show" id="allProdCount">') + 58, (body.indexOf('</span>', body.indexOf('<span class="product-count__title show" id="allProdCount">') + 58)));
+			let totalResults = extractData(body, '<span class="product-count__title show" id="allProdCount">', '</span>');
 			totalResults = parseInt(totalResults.replace(',', ''));
 			for (let i = 0; i < 24; i++) {
 				let itemIndex = body.indexOf('<div class="plp-pod__info">', body.indexOf('data-pos="' + i + '"', body.indexOf('<div id="products"')));
-				// console.log(itemIndex);
-				let name = extractData(body, '<span class="pod-plp__brand-name">Milwaukee</span>', '</a>', itemIndex);
+				let name = extractData(body, '<span class="pod-plp__brand-name">' + brand +'</span>', '</a>', itemIndex);
 				let modelNo = extractData(body, 'Model# &nbsp;', '</div>', itemIndex);
 				let price = extractData(body, '<span class="price__format">&#36;</span>', '<span', itemIndex);
 				let rating = extractData(body, '<span class="stars" rel="', '"', itemIndex);
@@ -41,12 +73,11 @@ function getToolListings(start = 0) {
 			}
 			// console.log(totalResults , toolListings.length);
 			if (totalResults > toolListings.length) {
-				console.log(totalResults , toolListings.length);
-				getToolListings(toolListings.length);
+				getToolListings(brand, toolListings.length);
 			}
 			else{
 				for (let i = 0; i < toolListings.length; i++) {
-					console.log(toolListings[i].allData);
+					console.log(toolListings[i].getName);
 				}
 			}
 	})
@@ -56,24 +87,4 @@ function extractData(body, startString, endString, start = 0) {
 	return body.substring(body.indexOf(startString, start) + startString.length, body.indexOf(endString, body.indexOf(startString, start) + startString.length));
 }
 
-getToolListings();
-
-
-
-
-//<span class="pod-plp__brand-name">Milwaukee</span>
-
-//https://www.homedepot.com/b/Tools/N-5yc1vZc1xyZzv?experienceName=default&Nao=0&Ns=None
-//https://www.homedepot.com/b/Tools/N-5yc1vZc1xyZzv?experienceName=default&Nao=24&Ns=None
-//https://www.homedepot.com/b/Tools/N-5yc1vZc1xyZzv?experienceName=default&Nao=48&Ns=None
-
-// request({
-// 	uri: "https://www.homedepot.com/b/Tools/Milwaukee/Special-Values/N-5yc1vZc1xyZ7Zzv",
-// }, function (error, response, body) {
-// 		fs.writeFile('data.html', body, function (err) {
-// 			if (err) throw err;
-// 			console.log('File is created successfully.');
-// 		  });
-// })
-
-//<span class="pod-plp__brand-name">Milwaukee</span>
+getToolListings('Milwaukee');
